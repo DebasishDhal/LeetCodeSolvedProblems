5a540358d42bfe4721abf1c81277394e84578590
DebasishDhal <31160148+DebasishDhal@users.noreply.github.com>
2023-12-12 12:49:05 +0530

    Update max_product_of_two_elements_of_array.py

--- two-pointer-method/max_product_of_two_elements_of_array.py
+++ two-pointer-method/max_product_of_two_elements_of_array.py
@@ -1,9 +1,9 @@
-'Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+'''Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
 Constraints:
 
 2 <= nums.length <= 500
 1 <= nums[i] <= 10^3
-'
+'''
 
 # Beats 65% of users in terms of speed and 10% in terms of memory
 class Solution:
