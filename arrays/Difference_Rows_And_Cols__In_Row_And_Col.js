a9c12a5a93bdde8fecad2908c9c348e0a8aaa3d7
DebasishDhal <31160148+DebasishDhal@users.noreply.github.com>
2023-12-14 11:54:51 +0530

    Create Difference_Rows_And_Cols__In_Row_And_Col.js

---
+++ arrays/Difference_Rows_And_Cols__In_Row_And_Col.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[][]} grid
+ * @return {number[][]}
+ You are given a 0-indexed m x n binary matrix grid.
+
+A 0-indexed m x n difference matrix diff is created with the following procedure:
+
+Let the number of ones in the ith row be onesRowi.
+Let the number of ones in the jth column be onesColj.
+Let the number of zeros in the ith row be zerosRowi.
+Let the number of zeros in the jth column be zerosColj.
+diff[i][j] = onesRowi + onesColj - zerosRowi - zerosColj
+Return the difference matrix diff.
+
+#Beats 100% of users in runtime.
+ */
+var onesMinusZeros = function(grid) {
+
+    len = grid.length
+    wid = grid[0].length
+
+    let rows = Array(len).fill(0)
+    let cols = Array(wid).fill(0)
+
+    for (let i = 0; i < len; i++){
+        for (let j = 0; j < wid; j++){
+            rows[i] += grid[i][j]
+            cols[j] += grid[i][j]
+        }
+    }
+
+    for (let i = 0; i < len; i++){
+        for (let j = 0; j < wid; j++){
+            grid[i][j] = 2*(rows[i]+cols[j]) - len - wid
+        }
+    }
+
+    return grid
+};
