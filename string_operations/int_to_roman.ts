0a491c419148e3b6786c8605c2c9ef64a96a2965
DebasishDhal <31160148+DebasishDhal@users.noreply.github.com>
2023-12-17 18:05:20 +0530

    Create int_to_roman.ts

---
+++ string_operations/int_to_roman.ts
@@ -0,0 +1,13 @@
+function intToRoman(num: number): string {
+       let dic = {1000:'M',900:'CM',500:'D',400:'CD',100:'C', 90:'XC',50:'L',40:'XL',10:'X',9:'IX',5:'V',4:'IV',1:'I'}
+
+    let roman = '';
+    for (let key of Object.keys(dic).reverse()) {
+        const number = parseInt(key); 
+        let quotient = Math.floor(num / number);
+        roman += dic[number].repeat(quotient);
+        num -= quotient * number;
+    }
+
+    return roman;
+};
