5deeb70bf9e3f72917b99bc769a4b44e302713a9
DebasishDhal <31160148+DebasishDhal@users.noreply.github.com>
2023-12-17 17:29:38 +0530

    Create int_to_roman.py

---
+++ string_operations/int_to_roman.py
@@ -0,0 +1,17 @@
+'''
+Beats 69% of users in runtime.
+'''
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        dic = {1000:'M',900:'CM',500:'D',400:'CD',100:'C', 90:'XC',50:'L',40:'XL',10:'X',9:'IX',5:'V',4:'IV',1:'I'}
+        dic = {item:value for item,value in dic.items() if item<=num} #This decreased the runtime to a big extent
+        roman = ''
+        for number,symbol in dic.items():
+            if num<number:
+                continue
+            quotient = num // number
+            roman += symbol*quotient
+            num -= quotient*number
+
+        return roman
