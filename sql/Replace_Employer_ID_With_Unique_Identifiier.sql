98e51e6bfbd38cc4540b530c0e4f2b44d36ec9bd
DebasishDhal <31160148+DebasishDhal@users.noreply.github.com>
2023-12-19 04:12:21 +0530

    Create Replace_Employer_ID_With_Unique_Identifiier.sql

---
+++ sql/Replace_Employer_ID_With_Unique_Identifiier.sql
@@ -0,0 +1,37 @@
+/* Write your MySQL query statement below
+Table: Employees
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains the id and the name of an employee in a company.
+ 
+
+Table: EmployeeUNI
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| unique_id     | int     |
++---------------+---------+
+(id, unique_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id and the corresponding unique id of an employee in the company.
+ 
+
+Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+
+Return the result table in any order.
+Good performance. As a rule of thumb, if some col is allowed to return null values in a join, then it is a low priority table and it should be the right table in a left join.
+*/
+SELECT 
+    Employees.name as name,
+    EmployeeUNI.unique_id as unique_id
+FROM 
+    Employees /*Employees is the main table here, while EmployeesUNI is the secondary*/
+LEFT JOIN
+    EmployeeUNI ON EmployeeUNI.id = Employees.id
